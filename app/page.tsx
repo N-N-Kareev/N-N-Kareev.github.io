ca3ddf5ab8397a532609d08eaf9025b70376cc4d
N-N-Kareev <89892916659k@gmail.com>
2024-08-22 01:56:23 +0300

    state commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/navbar";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Navbar />    
+      <Navbar />
     </main>
   );
 }
--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
     output: 'export',
-    basePath: '/coffee-place'
+    basePath: '/coffee/'
 };
 
 
