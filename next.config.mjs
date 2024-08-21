8b9b40087b4a6651d5587615f31477cb64012897
N-N-Kareev <89892916659k@gmail.com>
2024-08-22 00:36:52 +0300

    state commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: 'https://github.com/N-N-Kareev/coffee-palce'
+};
 
 export default nextConfig;
