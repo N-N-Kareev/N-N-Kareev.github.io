9aa3cc0681c366b5386dbf7724b395d0546667ed
N-N-Kareev <89892916659k@gmail.com>
2024-08-22 02:23:32 +0300

    updated config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
-// module.exports = {
-//     output: 'export',
-//     basePath: '/cofee/', // Замените 'your-repo-name' на имя вашего репозитория
-//   };
+module.exports = {
+    output: 'export',
+    basePath: '/coffee', // Замените 'your-repo-name' на имя вашего репозитория
+  };
 
 
