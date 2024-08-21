c24ae28c2cd2ff956fd1bd2ba9eb177ec003791d
N-N-Kareev <89892916659k@gmail.com>
2024-08-22 02:50:37 +0300

    Added image

--- app/pages/menu/page.tsx
+++ app/pages/menu/page.tsx
@@ -1,18 +1,39 @@
 // pages/menu.js
 import React from 'react';
+import Image from 'next/image';
+
 
 const Menu = () => {
+  // const placeOrder = () => {
+  //   const order = prompt('Введите номер напитка:');
+  //   if (order) {
+  //     alert(`Вы заказали: ${order}`);
+  //     // Здесь можно добавить логику для отправки заказа на сервер или в Telegram
+  //   }
+  // };
+
+  const menuItems = [
+    { name: 'Эспрессо', price: '50 руб.', image: '/express.jfif' },
+    { name: 'Американо', price: '70 руб.', image: '/americano.jfif' },
+    { name: 'Капучино', price: '100 руб.', image: '/capo.jfif' },
+    { name: 'Латте', price: '120 руб.', image: '/latte.jfif' },
+    { name: 'Мокко', price: '150 руб.', image: '/moco.jfif' },
+  ];
+
   return (
     <div>
-      <h1>Menu</h1>
-      <ul>
-        <li>Espresso - $2.50</li>
-        <li>Cappuccino - $3.00</li>
-        <li>Latte - $3.50</li>
-        <li>Mocha - $4.00</li>
-      </ul>
+        <h1>Меню кофейни</h1>
+        <div className="menu">
+          {menuItems.map((item: any, index) => (
+            <div key={index} className="menu-item">
+              <Image src={item.image} alt={item.name} width={150} height={150} />
+              <div>{index + 1}. {item.name} - {item.price}</div>
+            </div>
+          ))}
+        </div>
+        {/* <button className="order-button" onClick={() => {}}>Заказать</button> */}
     </div>
   );
-};
+}
 
 export default Menu;
