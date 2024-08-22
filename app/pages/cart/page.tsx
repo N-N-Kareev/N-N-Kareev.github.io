a61bb8a8f7ad02fcb9d792c5df5b22b1911d1b7f
Nikolay-Kareev <n.kareev@redlight.group>
2024-08-22 13:43:10 +0300

    State commit

--- app/pages/cart/page.tsx
+++ app/pages/cart/page.tsx
@@ -2,83 +2,17 @@
 
 import { useState, useEffect, Suspense } from 'react';
 import { useSearchParams } from 'next/navigation'
+import Cart from '@/app/components/cart/cart';
 
 
- const Cart = ()  =>{
-
-    const [cart, setCart]: any = useState([]);
-    const searchParams = useSearchParams()
-    const stateCart = searchParams.get('cart')
-
-
-  
-    useEffect(() => {
-      const cartString: any = stateCart;
-      const savedCart = JSON.parse(decodeURIComponent(cartString) as any) || [];
-      setCart(savedCart);
-    }, []);
-  
-    useEffect(() => {
-      localStorage.setItem('cart', JSON.stringify(cart));
-    }, [cart]);
-  
-    const removeFromCart = (item: any) => {
-      const updatedCart = cart.filter((cartItem: any) => cartItem.name !== item.name);
-      setCart(updatedCart);
-    };
-  
-    const increaseQuantity = (item: any) => {
-      setCart(
-        cart.map((cartItem: any) =>
-          cartItem.name === item.name
-            ? { ...cartItem, quantity: cartItem.quantity + 1 }
-            : cartItem
-        )
-      );
-    };
-  
-    const decreaseQuantity = (item: any) => {
-      const updatedCart = cart.map((cartItem: any) =>
-        cartItem.name === item.name
-          ? { ...cartItem, quantity: cartItem.quantity - 1 }
-          : cartItem
-      );
-      setCart(updatedCart.filter((cartItem: any) => cartItem.quantity > 0));
-    };
-  
-    const totalPrice = cart.reduce((total: any, item: any) => total + item.price * item.quantity, 0);
-  
-
+ const CartPage = ()  =>{
   return (
     <Suspense fallback={<div>Loading...</div>}>
-    <div>
-        <h1>Корзина</h1>
-        {cart.length === 0 ? (
-          <p>Ваша корзина пуста.</p>
-        ) : (
-          <div>
-            {cart.map((item: any, index: number) => (
-              <div key={index} className="cart-item">
-                <img src={item.image} alt={item.name} width={100} height={100} />
-                <div>{item.name} - {item.price} руб.</div>
-                <div className="quantity-controls">
-                  <button onClick={() => decreaseQuantity(item)}>-</button>
-                  <span>{item.quantity}</span>
-                  <button onClick={() => increaseQuantity(item)}>+</button>
-                </div>
-                <button onClick={() => removeFromCart(item)}>Удалить</button>
-              </div>
-            ))}
-            <div className="total-price">
-              Общая стоимость: {totalPrice} руб.
-            </div>
-          </div>
-        )}
-    </div>
+      <Cart/>
   </Suspense>
 
 
   );
 }
 
-export default Cart;
+export default CartPage;
