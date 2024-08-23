9dbca0f434f9dfb1f4be3778bdf664892b427719
N-N-Kareev <89892916659k@gmail.com>
2024-08-23 04:05:59 +0300

    Added Mapbox

--- app/components/map/index.tsx
+++ app/components/map/index.tsx
@@ -1,48 +1,75 @@
+//pk.eyJ1IjoicmljaC1nb3NsZW5nIiwiYSI6ImNsaGFoZ3kxeDBoaXozcGx0ajV4OTRmNjYifQ.qXzNY9j52Qk0u91foSqKPA
+
 "use client";
 
-import  { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import ReactMapGL, { Marker, Popup } from 'react-map-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
 import Image from 'next/image';
 
-import L from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import 'leaflet/dist/leaflet.css';
-
-
-const CoffeeShopMap = () => {
-  const position:  L.LatLngExpression  = [47.2220375, 38.9184931];
-  const coffeeIcon = new L.Icon({
-    iconUrl: '/marker.png', 
-    iconSize: [38, 38], 
-    iconAnchor: [19, 38], 
-    popupAnchor: [0, -38], 
+const CoffeeShopMap: React.FC = () => {
+  const [viewport, setViewport] = useState({
+    latitude: 47.2141,
+    longitude: 38.9305578,
+    zoom: 13,
+    width: '100%',
+    height: '250px',
   });
 
-  const [isClient, setIsClient] = useState(false);
+  const [popupInfo, setPopupInfo] = useState(null);
 
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
-
-  if (!isClient) {
-    return null;
-  }
+  const handleClick = (event: any) => {
+    setPopupInfo({
+      //@ts-ignore
+      latitude: 47.2141,
+      longitude: 38.9305578,
+    });
+  };
 
   return (
-    <MapContainer center={position} zoom={22} style={{ height: '100%', width: '100%' }}>
-      <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution=''
-      />
-      <Marker position={position} icon={coffeeIcon}>
-        <Popup>
-         <div className='marker-wrapper'>
-         <Image src={'/coffee.jpg'} width={100} height={100} alt={''}/>
-
-          Coffee Break <br /> Уютная кофейня в центре города.
-         </div>
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <div style={{ height: '250px', borderRadius: '10px', overflow: 'hidden' }}>
+      <ReactMapGL
+        {...viewport}
+        mapboxAccessToken={'pk.eyJ1IjoicmljaC1nb3NsZW5nIiwiYSI6ImNsaGFoZ3kxeDBoaXozcGx0ajV4OTRmNjYifQ.qXzNY9j52Qk0u91foSqKPA'}
+        //@ts-ignore
+        onViewportChange={(nextViewport: any) => setViewport(nextViewport)}
+        onClick={handleClick}
+        mapStyle="mapbox://styles/mapbox/streets-v11"
+      >
+        <Marker
+          latitude={47.2141}
+          longitude={38.9305578}
+          //@ts-ignore
+          offsetLeft={-19}
+          offsetTop={-38}
+        >
+          <img src="/marker.png" alt="Marker" style={{ width: 38, height: 38 }} />
+        </Marker>
+        {popupInfo && (
+          <Popup
+          //@ts-ignore
+            tipSize={5}
+            anchor="top"
+            //@ts-ignore
+            longitude={popupInfo.longitude}
+            //@ts-ignore
+            latitude={popupInfo.latitude}
+            closeOnClick={false}
+            onClose={(event) => setPopupInfo({
+              //@ts-ignore
+              latitude: 47.2141,
+              //@ts-ignore
+              longitude: 38.9305578,
+            })}
+          >
+            <div className='marker-wrapper'>
+            <Image src={'/coffee.jpg'} width={100} height={100} alt=''/>
+            Мока Лайт <br /> Уютная кофейня в центре города.
+            </div>
+          </Popup>
+        )}
+      </ReactMapGL>
+    </div>
   );
 };
 
--- app/pages/about-us/page.tsx
+++ app/pages/about-us/page.tsx
@@ -1,13 +1,26 @@
-"use client";
 
-import About from "@/app/components/about";
+import CoffeeShopMap from "@/app/components/map";
 import { Suspense } from "react";
 
 const AboutPage = () => {
-    return (
-        <About/>
-    )
-  };
-  
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+ <section>
+      <h1>О нас</h1>
+      <p>
+        Добро пожаловать в Мока Лайт! Мы — уютная кофейня, где аромат свежесваренного кофе и теплый прием создают идеальную атмосферу для приятного отдыха. В нашем меню вы найдете широкий выбор кофе, чая, свежей выпечки и других вкусностей, которые поднимут вам настроение и подарят заряд энергии. Приходите к нам, чтобы насладиться вкусным кофе и приятной атмосферой!
+      </p>
+      <div>
+        <h2>Наше местоположение</h2>
+        <div className="map-wrapper">
+          <Suspense fallback={<div>Loading...</div>}>
+            <CoffeeShopMap />
+          </Suspense>
+        </div>
+      </div>
+    </section>
+    </Suspense>
+  );
+};
 
 export default AboutPage;
